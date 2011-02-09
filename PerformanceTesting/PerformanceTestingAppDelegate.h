6d95b2bb94f56e76e8c416e6b52fb67f96a03bc1
Madis Nõmme <madis@madisMBP.local>
2011-02-09 08:36:41 +0200

    Initial Commit

---
+++ PerformanceTesting/PerformanceTestingAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  PerformanceTestingAppDelegate.h
+//  PerformanceTesting
+//
+//  Created by Madis Nõmme on 2/9/11.
+//  Copyright 2011 Indilo Wireless. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PerformanceTestingAppDelegate : NSObject <UIApplicationDelegate> {
+@private
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
